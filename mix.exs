77242703c64e461587d4f28b54689ca65295c12f
JuliaMathias <juliamss@gmail.com>
2022-05-30 17:25:46 -0300

    add exdocs

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Move.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_doc, ">= 0.0.0", runtime: false, only: [:docs, :dev]}
     ]
   end
 
