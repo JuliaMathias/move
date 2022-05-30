6427f906dc718616f54cd2844abc576f2362dc92
JuliaMathias <juliamss@gmail.com>
2022-05-30 18:11:41 -0300

    migration and initial model

---
+++ lib/move/library/exercise.ex
@@ -0,0 +1,77 @@
+defmodule Move.Library.Exercise do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :id, autogenerate: true}
+
+  schema "exercises" do
+    field :body_part, Ecto.Enum,
+      values: [
+        :arms,
+        :back,
+        :calves,
+        :core,
+        :feet,
+        :glutes,
+        :hands,
+        :hips,
+        :legs,
+        :neck,
+        :shoulders,
+        :thighs,
+        :wrists
+      ]
+
+    field :description, :string
+    field :duration_in_seconds, :integer
+
+    field :equipment, Ecto.Enum,
+      values: [
+        :ankle_weights,
+        :dumbbells,
+        :elastic_bands,
+        :finger_elastics,
+        :mini_resistance_bands,
+        :none,
+        :resistance_bands
+      ]
+
+    field :history, :map
+    field :location, Ecto.Enum, values: [:bedroom, :irrelevant, :living_room]
+    field :name, :string
+    field :reps, :integer, default: 1
+    field :sets, :integer, default: 1
+    field :weekly_frequency, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(exercise, attrs) do
+    exercise
+    |> cast(attrs, [
+      :name,
+      :description,
+      :reps,
+      :sets,
+      :duration_in_seconds,
+      :weekly_frequency,
+      :location,
+      :body_part,
+      :equipment,
+      :history
+    ])
+    |> validate_required([
+      :name,
+      :description,
+      :reps,
+      :sets,
+      :duration_in_seconds,
+      :weekly_frequency,
+      :location,
+      :body_part,
+      :equipment,
+      :history
+    ])
+  end
+end
---
+++ priv/repo/migrations/20220530203622_create_exercises.exs
@@ -0,0 +1,22 @@
+defmodule Move.Repo.Migrations.CreateExercises do
+  use Ecto.Migration
+
+  def change do
+    create table(:exercises) do
+      add(:name, :string, null: false)
+      add(:description, :string)
+      add(:reps, :integer, null: false)
+      add(:sets, :integer, null: false)
+      add(:duration_in_seconds, :integer)
+      add(:weekly_frequency, :integer)
+      add(:location, :string, null: false)
+      add(:body_part, :string, null: false)
+      add(:equipment, :string, null: false)
+      add(:history, :map)
+
+      timestamps()
+    end
+
+    create(unique_index(:exercises, :name))
+  end
+end
